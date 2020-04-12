4fca335356251fd21dd17fef14b9fe0f97be25a7
Christophe Beveraggi <beve@croll.fr>
2020-04-12 23:57:12 +0200

    Landing text improvements

--- src/blocks/landing.js
+++ src/blocks/landing.js
@@ -10,21 +10,43 @@ const grid = css`
 `
 
 const caption = (theme) => css`
-  display: flex;
-  flex-direction: column;
-  grid-column: 2 / span 12 ;
   grid-row: 1;
-  text-transform: uppercase;
+  grid-column: 2 / span 12 ;
+  display: flex;
+  flex-flow: column wrap;
+  flex-direction: column;
+  margin: auto 0;
   align-items: center;
-  justify-content: center;
+  text-transform: uppercase;
   font-size: 95px;
+  font-size: 5.95em;
   line-height: 106px;
+  line-height: 1.12em;
   letter-spacing: 2px;
-  margin-top: -120px;
+  // margin-top: -120px;
   font-weight: bold;
   span {
     color: ${theme.colors.title};
   }
+  @media (max-width: 1366px) {
+    display: block;
+    margin-left: -0.2em;
+  }
+  @media (max-width: 1000px) {
+    max-width: 700px;
+  }
+  @media (max-width: 800px) {
+    max-width: 500px;
+  }
+`
+
+const button = (theme) => css`
+  display: flex;
+  margin: auto 0;
+  grid-row: 1;
+  grid-column: 13 / span 2;
+  box-sizing: border-box;
+  transform: translateX(calc(-50%));
 `
 
 const desc = (theme) => css`
@@ -32,7 +54,7 @@ const desc = (theme) => css`
   flex-direction: column;
   justify-content: flex-end;
   grid-row: 1;
-  grid-column: 4 / span 14 ;
+  grid-column: 4 / -2 ;
   margin-bottom: 50px;
   font-size: 22px;
   font-weight: 300;
@@ -42,30 +64,15 @@ const desc = (theme) => css`
   }
 `
 
-const buttonContainer = css`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  margin-top: -120px;
-  grid-row: 1;
-  grid-column: 13 / span 2;
-`
-
-const button = (theme) => css`
-  transform: translateX(calc(-50%));
-`
-
 export default () => {
   return (
     <>
       <Grid gridCss={grid} drawCols={14}>
         <div css={caption}>
           <div>Make dev.</div>
-          <span>great again !</span>
+          <span>great again&nbsp;!</span>
         </div>
-        <div css={buttonContainer}>
-          <AnimationButton customCss={button} />
-        </div>
+        <AnimationButton customCss={button} />
         <div css={desc}>
           <div>Hello, je m'appelle <span>Christophe Beveraggi</span></div>
           <div>Je suis développeur depuis 20 ans, concepteur d'objet amusants, et maker.</div>
